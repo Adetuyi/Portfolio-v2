303ea1bf603d3f6deaa7a6215c51291b1bff158b
Adetuyi <seyispecial17@gmail.com>
2025-07-31 00:13:47 +0100

    Route to new portfolio

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,12 @@ import HeroImage from '../components/HeroImage';
 import MiniWorks from '../components/MiniWorks';
 import Navbar from '../components/Navbar';
 import Contact from './contact';
+import { useEffect } from 'react';
 
 export default function Home({ service_id, template_id, user_id }) {
+	useEffect(() => {
+		window.location.href = 'http://adetuyi.vercel.app/';
+	}, []);
 	return (
 		<>
 			<Navbar />
@@ -18,16 +22,17 @@ export default function Home({ service_id, template_id, user_id }) {
 					<h2>Quick Bio</h2>
 
 					<p>
-						I am an experienced web developer passionate about creating great user experiences through tasteful and intuitive UIs that look
-						stunning on both desktop and mobile screens. I have a curiosity about new technologies and a constant desire to learn new things and
-						perfect them as much as possible.
+						I am an experienced web developer passionate about creating great user experiences through tasteful and intuitive
+						UIs that look stunning on both desktop and mobile screens. I have a curiosity about new technologies and a constant
+						desire to learn new things and perfect them as much as possible.
 					</p>
 					<p>
-						Programming is a fun and exciting concept to me. You can <Link href="/about">read more</Link> about me or look up some of{' '}
-						<Link href="/works">my works</Link>. Here&apos;s a link to my <a href="/files/Resume.pdf">resume</a>
+						Programming is a fun and exciting concept to me. You can <Link href="/about">read more</Link> about me or look up
+						some of <Link href="/works">my works</Link>. Here&apos;s a link to my <a href="/files/Resume.pdf">resume</a>
 					</p>
 					<p>
-						Wanna talk ? You can email me at <a href="mailto:seyispecial17@gmail.com">seyispecial17@gmail.com</a> or message me on linkedIn at{' '}
+						Wanna talk ? You can email me at <a href="mailto:seyispecial17@gmail.com">seyispecial17@gmail.com</a> or message me
+						on linkedIn at{' '}
 						<a href="https://www.linkedin.com/in/seyi-adetuyi-054b56206/" target="_blank" rel="noreferrer">
 							Seyi Adetuyi
 						</a>
