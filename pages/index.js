4c8eb8b748874c483e7f4be372d0c4e8fd14ac3f
Oluwaseyi Adetuyi <seyispecial17@gmail.com>
2026-02-27 13:15:50 +0100

    Update redirect URL to use HTTPS

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import { useEffect } from 'react';
 
 export default function Home({ service_id, template_id, user_id }) {
 	useEffect(() => {
-		window.location.href = 'http://adetuyi.vercel.app/';
+		window.location.href = 'https://adetuyi.vercel.app/';
 	}, []);
 	return (
 		<>
